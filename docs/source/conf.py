cfa81d6c7353a580d1fc072a17e5451f1e3b70d3
Rıza Özçelik <rizaozcelik96@gmail.com>
2022-07-05 23:10:41 +0200

    bug fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,8 +4,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../pydebiaseddta'))
+# sys.path.insert(0, os.path.abspath('../..'))
+# sys.path.insert(0, os.path.abspath('../../pydebiaseddta'))
 sys.path.insert(0, os.path.abspath('../../pydebiaseddta/pydebiaseddta'))
 # sys.path.insert(0, os.path.abspath('../pydebiaseddta'))
 
