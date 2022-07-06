3e68e7b4f6c0a1f00ce59a2c8320268ba90eb1db
Rıza Özçelik <rizaozcelik96@gmail.com>
2022-07-06 11:22:51 +0200

    try combinations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,8 +4,7 @@
 import os
 import sys
 
-# sys.path.insert(0, os.path.abspath('../../pydebiaseddta'))
-# sys.path.insert(0, os.path.abspath('../../pydebiaseddta/pydebiaseddta'))
+sys.path.insert(0, os.path.abspath('../../pydebiaseddta'))
 
 
 project = 'pydebiaseddta'
@@ -39,5 +38,3 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
-
-master_doc = 'index'
