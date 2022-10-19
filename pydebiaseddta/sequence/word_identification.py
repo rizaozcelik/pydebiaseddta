94e2c4bb3f8baf479e1d88b3714b84e67e7bbe51
Rıza Özçelik <rizaozcelik96@gmail.com>
2022-10-19 22:06:14 +0200

    commented word identifier

--- pydebiaseddta/sequence/word_identification.py
+++ pydebiaseddta/sequence/word_identification.py
@@ -12,13 +12,36 @@ FILE_EXTENSION = ".json"
 
 
 class WordIdentifier:
+    """A versatile class to identify biomolecule words in biomolecule strings. 
+    ``WordIdentifier`` leverages the Byte Pair Encoding algorithm implemented in the``tokenizers`` library
+    to learn biomolecule vocabularies and segment biomolecule strings into their words. 
+    """    
     def __init__(self, vocab_size: int):
+        """Creates a ``WordIdentifier`` instance.
+
+        Parameters
+        ----------
+        vocab_size : int
+            Size of the biomolecule vocabulary.
+        """        
         self.vocab_size = vocab_size
         self.tokenizer = Tokenizer(BPE())
         self.tokenizer.pre_tokenizer = Whitespace()
 
     @classmethod
     def from_file(cls, loadpath: str):
+        """Loads a ``WordIdentifier`` from a file.
+
+        Parameters
+        ----------
+        loadpath : str
+            Path to the ``WordIdentifier`` file.
+
+        Returns
+        -------
+        WordIdentifier
+            Previously saved ``WordIdentifier``
+        """        
         if not loadpath.endswith(FILE_EXTENSION):
             loadpath = loadpath + FILE_EXTENSION
 
@@ -29,6 +52,13 @@ class WordIdentifier:
         return instance
 
     def train(self, corpus_path: str):
+        """Learns a biomolecule vocabulary from a file of biomolecule strings using Byte Pair Encoding Algorithm. 
+
+        Parameters
+        ----------
+        corpus_path : str
+            Path to the corpus of biomolecule strings. The corpus file must contain a biomolecule string per line.
+        """        
         trainer = BpeTrainer(
             vocab_size=self.vocab_size, special_tokens=["[PAD]"]
         )
@@ -38,11 +68,39 @@ class WordIdentifier:
                 f"Warning: The iterations stopped before the desired vocab size is reached. Learned vocab size={self.tokenizer.get_vocab_size()}. Desired size={self.vocab_size}"
             )
 
-    def tokenize_sequences(self, sequences: List[str]):
+    def tokenize_sequences(self, sequences: List[str]) -> List[List[str]]:
+        """Segments a List of biomolecule strings into biomolecule words via the learned vocabulary.
+
+        Parameters
+        ----------
+        sequences : List[str]
+            The List of biomolecule strings.
+
+        Returns
+        -------
+        List[List[str]]
+            List of biomolecule words of each input string.
+        """        
         encodings = self.tokenizer.encode_batch(sequences)
         return [encoding.tokens for encoding in encodings]
 
-    def encode_sequences(self, sequences: List[str], padding_len: int = None):
+    def encode_sequences(self, sequences: List[str], padding_len: int = None)-> List[List[int]]:
+        """Segments a List of biomolecule strings into biomolecule words via the learned vocabulary and 
+        returns the id of the biomolecule word, which is convenient to apply label encoding in the subsequent steps.
+        Padding support is also available to ease training deep learning possible.
+
+        Parameters
+        ----------
+        sequences : List[str]
+            The List of biomolecule strings.
+        padding_len : int, optional
+            The desired length of sequences, by default ``None``. No padding is applied when set to ``None``.
+
+        Returns
+        -------
+        List[List[int]]
+            List of the id of the biomolecule words of each input string.
+        """        
         encodings = self.tokenizer.encode_batch(sequences)
         if isinstance(padding_len, int):
             for encoding in encodings:
@@ -54,12 +112,37 @@ class WordIdentifier:
         return [encoding.ids for encoding in encodings]
 
     def save(self, savepath: str):
+        """Saves a ``WordIdentifier`` instance to disk.
+
+        Parameters
+        ----------
+        savepath : str
+            The path to dump the instance. File extension is added automatically.
+        """        
         if not savepath.endswith(FILE_EXTENSION):
             savepath = savepath + FILE_EXTENSION
         save_json(json.loads(self.tokenizer.to_str()), savepath)
 
 
-def load_chemical_word_identifier(vocab_size: int):
+def load_chemical_word_identifier(vocab_size: int) -> type[WordIdentifier]:
+    """A convenience function to load word vocabularies learned for SMILES strings in the study.
+    The possible vocabularies to load are for DeepDTA and BPE-DTA. 
+
+    Parameters
+    ----------
+    vocab_size : int
+        Size of the learned SMILES word vocabulary. The allowed values are 94 and 8000, for DeepDTA and BPE-DTA, respectively.
+
+    Returns
+    -------
+    type[WordIdentifier]
+        The ``WordIdentifier`` instance used by the DTA models.
+
+    Raises
+    ------
+    ValueError
+        If vocabulary size besides 94 and 8000 is passed, a ``ValueError`` is raised.
+    """    
     if vocab_size not in [94, 8000]:
         raise ValueError("Supported vocab sizes are 94 and 8000")
 
@@ -71,7 +154,25 @@ def load_chemical_word_identifier(vocab_size: int):
     return WordIdentifier.from_file(vocab_path)
 
 
-def load_protein_word_identifier(vocab_size: int):
+def load_protein_word_identifier(vocab_size: int)-> type[WordIdentifier]:
+    """A convenience function to load word vocabularies learned for amino-acid sequences in the study.
+    The possible vocabularies to load are for DeepDTA and BPE-DTA. 
+
+    Parameters
+    ----------
+    vocab_size : int
+        Size of the learned SMILES word vocabulary. The allowed values are 26 and 32000, for DeepDTA and BPE-DTA, respectively.
+
+    Returns
+    -------
+    type[WordIdentifier]
+        The ``WordIdentifier`` instance used by the DTA models.
+
+    Raises
+    ------
+    ValueError
+        If vocabulary size besides 26 and 32000 is passed, a ``ValueError`` is raised.
+    """    
     if vocab_size not in [26, 32000]:
         raise ValueError("Supported vocab sizes are 26 and 32000")
 
